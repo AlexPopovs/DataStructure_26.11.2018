ce6bfae18a9fc7a2df5a6529435fd86f99e06770
Alexlv <alexandrlv@gmail.com>
2018-11-26 23:05:47 +0200

    made some first changes to ArrayOfIntegers file

--- src/ArrayOfIntegers.java
+++ src/ArrayOfIntegers.java
@@ -17,27 +17,23 @@ public class ArrayOfIntegers {
 
             System.out.println("\nDo you want to enter another digit? Your Answers: 0 - yes, 1 - no");
 
-           // while (true) {      // "while" is used here because we need loop to make program able to terminate and we need to use "break".
+            int wantToContinue = in.nextInt();
 
-                int wantToContinue = in.nextInt();
+            if (wantToContinue == 0) {
 
-                if (wantToContinue == 0) {
+                System.out.println("Please input your digit for array");
+                int size1 = in.nextInt();
+                int[] user1 = new int[size1];
 
-                    System.out.println("Please input your digit for array");
-                    int size1 = in.nextInt();
-                    int[] user1 = new int[size1];
+                System.out.println("Program successfully initialized an array");
+                System.out.println("\nDo you want to enter another digit? Your Answers: 0 - yes, 1 - no");
+                int size2 = in.nextInt();
+                int[] user2 = new int[size2];
 
-                    System.out.println("Program successfully initialized an array");
-                    System.out.println("\nDo you want to enter another digit? Your Answers: 0 - yes, 1 - no");
-                    int size2 = in.nextInt();
-                    int[] user2 = new int[size2];
-
-                } else if (wantToContinue == 1) {
-                    System.out.println("Thank you for using our product and Good bye!");
-                    break;
-                }
-
-            //}
+            } else if (wantToContinue == 1) {
+                System.out.println("Thank you for using our product and Good bye!");
+                break;
+            }
         }
     }
 }
